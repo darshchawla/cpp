d7e960392fa13ebdd41fd88ab45e8d56728db8e1
Darsh Chawla <darshdchawla@gmail.com>
2026-07-20 23:48:56 +0530

    square of a number

---
+++ basics/squareOfaNumber.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout << "Enter a number : ";
+    cin >> a;
+    int b = a * a;
+    cout << "The square of the given number is : ";
+    cout << b << endl;
+}
