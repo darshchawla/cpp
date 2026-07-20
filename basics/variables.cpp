7c30936741214d547ad1bbd9154a08236fe58f50
Darsh Chawla <darshdchawla@gmail.com>
2026-07-20 23:50:17 +0530

    variables

---
+++ basics/variables.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x; // declaration
+    x = 5; // initialization
+    int y;
+    y = 7;
+    cout << x + y << endl;
+    cout << x * y;
+}
