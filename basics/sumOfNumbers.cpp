031af1bcfd36b6c6392265509d03bddc5fa46c19
Darsh Chawla <darshdchawla@gmail.com>
2026-07-20 23:50:00 +0530

    sum of numbers

---
+++ basics/sumOfNumbers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout << "Enter first number : ";
+    cin >> a;
+    int b;
+    cout << "Enter second number : ";
+    cin >> b;
+    int c = a + b;
+    cout << "The sum of both the numbers is : ";
+    cout << c << endl;
+}
