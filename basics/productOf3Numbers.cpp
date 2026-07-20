0c5239018f5fc39c50290802df2d5774a2f2ddb1
Darsh Chawla <darshdchawla@gmail.com>
2026-07-20 23:48:18 +0530

    product of three numbers

---
+++ basics/productOf3Numbers.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout << "Enter first number : ";
+    cin >> a;
+    int b;
+    cout << "Enter second number : ";
+    cin >> b;
+    int c;
+    cout << "Enter third number : ";
+    cin >> c;
+    int d = a * b * c;
+    cout << "The product of the given three numbers is : ";
+    cout << d << endl;
+}
