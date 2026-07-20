62613b15c5ca18c5ddb8f9777c4de3818c6e6e35
Darsh Chawla <darshdchawla@gmail.com>
2026-07-20 23:50:54 +0530

    updation of variables

---
+++ basics/updationOfVariables.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x = 5;
+    cout << x << endl;
+    x = 7;
+    cout << x << endl;
+    x += 3;
+    cout << x << endl;
+    x *= 5;
+    cout << x << endl;
+    x -= 15;
+    cout << x << endl;
+}
