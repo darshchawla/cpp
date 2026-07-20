6f55d511f4d6768c19a7a964d5fc92e9f7fe93e8
Darsh Chawla <darshdchawla@gmail.com>
2026-07-20 18:40:15 +0530

    first program

---
+++ basics/firstprogram.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Hello World.";
+    // cout<<"\n";
+    cout << endl;
+    cout << "Hello Universe.";
+    // cout<<"Hello World"<<endl<<"Hello Universe";
+}
