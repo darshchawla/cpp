db5bb2092b783a90bfab36cca3db6ed886d08bc7
Darsh Chawla <darshdchawla@gmail.com>
2026-07-21 11:34:33 +0530

    ascii values

---
+++ basics/asciivalues.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char ch;
+    cout << "Enter a character : ";
+    cin >> ch;
+    int a = (int)ch;
+    cout << "The ASCII value of the given character is : ";
+    cout << a << endl;
+}
