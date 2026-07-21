296daecc0a8b045ac1a745870ac5fa5230ab9a48
Darsh Chawla <darshdchawla@gmail.com>
2026-07-21 11:40:52 +0530

    int data type to char data type

---
+++ basics/intToChar.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout << "Enter a value : ";
+    cin >> a;
+    char ch = (char)a;
+    cout << "The character of ascii value given by the user is : ";
+    cout << ch << endl;
+}
