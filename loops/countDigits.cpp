1ade700b9a1dd6079a31067b336756b5e99516e9
Darsh Chawla <darshdchawla@gmail.com>
2026-07-22 22:54:44 +0530

    count digits

---
+++ loops/countDigits.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter a number : ";
+    cin >> n;
+    int count = 0;
+    while (n != 0)
+    {
+        n = n / 10;
+        count++;
+    }
+    cout << "The number of digits in the given number is : ";
+    cout << count << endl;
+}
