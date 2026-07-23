1c68ae42944793cf170965f021e5fca65ef69143
Darsh Chawla <darshdchawla@gmail.com>
2026-07-23 08:56:15 +0530

    reverse of a number

---
+++ loops/reverseOfaNumber.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter a number : ";
+    cin >> n;
+    int reverse = 0;
+    while (n != 0)
+    {
+        int lastDigit = n % 10;
+        reverse = reverse * 10;
+        reverse = reverse + lastDigit;
+        n = n / 10;
+    }
+    cout << "The reverse of the number is : ";
+    cout << reverse;
+}
