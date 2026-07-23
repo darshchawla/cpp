742bc582c099c1c8319c5467201a50b4f6dfe819
Darsh Chawla <darshdchawla@gmail.com>
2026-07-24 01:12:58 +0530

    basics of functions

---
+++ functionsAndPointer/basicsFunctions.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+void greet()
+{
+    cout << "Good Morning!!" << endl;
+    cout << "How Are You??" << endl;
+}
+int main()
+{
+    cout << "Hey" << endl;
+    greet();
+    cout << "Hey" << endl;
+    greet();
+}
