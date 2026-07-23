edee8419d5409f1a7195c292319431b7eb0a592e
Darsh Chawla <darshdchawla@gmail.com>
2026-07-24 01:20:40 +0530

    question 1

---
+++ functionsAndPointer/question1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+void usa()
+{
+    cout << "You are in USA." << endl;
+    return;
+}
+void india()
+{
+    cout << "You are in India." << endl;
+    return;
+}
+int main()
+{
+    india();
+    usa();
+}
