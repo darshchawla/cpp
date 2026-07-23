3faf48afc8962152d5be27ba457ba5c636b6ac93
Darsh Chawla <darshdchawla@gmail.com>
2026-07-24 01:58:15 +0530

    library functions

---
+++ functionsAndPointer/libraryFunctions.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter a number : ";
+    cin >> n;
+    sqrt(n);
+    cout << "The square root of the given number : ";
+    cout << sqrt(n);
+}
