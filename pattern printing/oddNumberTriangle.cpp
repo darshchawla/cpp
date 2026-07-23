f66a530f1be79d0143ca8bccbbef8ecd1f582d27
Darsh Chawla <darshdchawla@gmail.com>
2026-07-23 13:31:37 +0530

    odd number triangle

---
+++ pattern printing/oddNumberTriangle.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int l;
+    cout << "Enter number of rows : ";
+    cin >> l;
+    for (int i = 1; i <= l; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << 2 * j - 1 << " ";
+        }
+        cout << endl;
+    }
+}
