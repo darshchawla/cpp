0f213f29bb6308d811c92d13b85adb6cd0d6ba1c
Darsh Chawla <darshdchawla@gmail.com>
2026-07-23 13:14:17 +0530

    number triangle reverse

---
+++ pattern printing/numberTriangleReverse.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int l;
+    cout << "Enter number of lines : ";
+    cin >> l;
+    for (int i = 1; i <= l; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << l + 1 - j << " ";
+        }
+        cout << endl;
+    }
+}
