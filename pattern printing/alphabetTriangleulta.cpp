8ca53d9a5645684eeffbf667cdcad2883d329ffb
Darsh Chawla <darshdchawla@gmail.com>
2026-07-23 13:08:22 +0530

    alphabet triangle ulta

---
+++ pattern printing/alphabetTriangleulta.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int l;
+    cout << "Enter number of lines : ";
+    cin >> l;
+    for (int i = 1; i <= l; i++)
+    {
+        for (int j = 1; j <= l + 1 - i; j++)
+        {
+            cout << (char)(j + 64) << " ";
+        }
+        cout << endl;
+    }
+}
