c4ecafc42aaf3e6f18a44eee245511158bf470c1
Darsh Chawla <darshdchawla@gmail.com>
2026-07-25 00:01:24 +0530

    sum of all elements in the array

---
+++ arrays/sumofAllElements.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter size of the array : ";
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i <= n - 1; i++)
+    {
+        int a = i + 1;
+        cout << endl;
+        cout << "Enter element number " << a << " : ";
+        cin >> arr[i];
+    }
+    cout << endl;
+    cout << "The array is : ";
+    for (int i = 0; i <= n - 1; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    cout << endl;
+    int sum = 0;
+    cout << "The sum of all the elements of the array is : ";
+    for (int i = 0; i <= n - 1; i++)
+    {
+        sum = sum + arr[i];
+    }
+    cout << sum << endl;
+}
