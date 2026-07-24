830b57edb4f1c1932329ee2bbe9a2ebef15dadfa
Darsh Chawla <darshdchawla@gmail.com>
2026-07-24 14:28:04 +0530

    printing numbers from 1 to 'n'

---
+++ recursion/printingNumbers1to'n'.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+void print(int x, int n)
+{
+    if (x ==n+1)
+        return;
+    cout << x << endl;
+    print(x+1,n);
+}
+int main()
+{
+    int n;
+    cout << "Enter a number : ";
+    cin >> n;
+    print(1,n);
+}
