a67e808007717199abe1496a3e53a00c45af1f37
Darsh Chawla <darshdchawla@gmail.com>
2026-07-25 08:59:25 +0530

    pop_back function

---
+++ strings/popback.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string str = "Hello";
+    cout << str << endl;
+    str.pop_back();
+    cout << str << endl;
+    str.pop_back();
+    cout << str << endl;
+}
