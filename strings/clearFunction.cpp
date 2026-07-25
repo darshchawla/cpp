f4d762498dbda2007829be54207728efd8b6c2ec
Darsh Chawla <darshdchawla@gmail.com>
2026-07-25 09:07:52 +0530

    clear function

---
+++ strings/clearFunction.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string str = "Darsh.";
+    cout << "The string is : " << str << endl << "The length of the string is : " << str.length() << endl;
+    cout << endl;
+    str.clear();
+    cout << "The string is : " << str << endl << "The length of the string is : " << str.length() << endl;
+}
