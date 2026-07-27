37be9b8a5e473e3df996a391be93299f36a44d79
Darsh Chawla <darshdchawla@gmail.com>
2026-07-27 22:39:29 +0530

    volume of a cube

---
+++ basics/volumeOfCube.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float s;
+    cout << "Enter side of the cube : ";
+    cin >> s;
+
+    cout << endl;
+
+    float volume = s * s * s;
+    cout << "The Volume of the Cube is : ";
+    cout << volume << endl;
+}
