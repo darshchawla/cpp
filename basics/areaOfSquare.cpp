206953ae242df2f357c7869afa4486d6334f22da
Darsh Chawla <darshdchawla@gmail.com>
2026-07-27 15:14:47 +0530

    area of square

---
+++ basics/areaOfSquare.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int s;
+    cout << "Enter side of square : ";
+    cin >> s;
+
+    cout << endl;
+
+    int area;
+    area = s * s;
+    cout << "The area of the square is : " << area << endl;
+}
