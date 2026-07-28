efabd2f2c84a691a14064e8ebf5a9907c9e6c2b1
Darsh Chawla <darshdchawla@gmail.com>
2026-07-28 19:26:18 +0530

    multi set

---
+++ STL/multiset.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <set>
+using namespace std;
+int main()
+{
+    multiset<int> s;
+
+    s.insert(1);
+    s.insert(2);
+    s.insert(3);
+    s.insert(4);
+    s.insert(5);
+
+    s.insert(1);
+    s.insert(2);
+    s.insert(3);
+
+    for (auto value : s)
+    {
+        cout << value << " ";
+    }
+
+    cout << endl;
+}
