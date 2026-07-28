8277c1c6ba2514720159b7be01eaf1e86e62dfcc
Darsh Chawla <darshdchawla@gmail.com>
2026-07-28 18:44:44 +0530

    map

---
+++ STL/map.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <map>
+using namespace std;
+int main()
+{
+    map<string, int> m;
+
+    m["TV"] = 100;
+    m["Laptop"] = 100;
+    m["Mobile Phones"] = 50;
+    m["Tablet"] = 120;
+    m["Watch"] = 50;
+
+    m.insert({"Camera", 70}); // or m.emplace
+
+    for (auto p : m)
+    {
+        cout << p.first << " - " << p.second << endl
+             << endl;
+    }
+}
