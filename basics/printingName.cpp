9800b37b1b3a658d3c1be74fa9abb2ac09d1c167
Darsh Chawla <darshdchawla@gmail.com>
2026-07-29 14:38:11 +0530

    printing name

---
+++ basics/printingName.cpp
@@ -0,0 +1,11 @@
+#include <iostream> // <iostream> is a library which allows you to take input and produces an output.
+using namespace std;
+
+int main()
+{
+    cout << "Hey Everyone!!" << endl;
+
+    cout << endl;
+
+    cout << "My name is Darsh Chawla." << endl;
+}
