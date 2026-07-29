02564fbcc66fa40768537579f7ea826f0ed379b0
Darsh Chawla <darshdchawla@gmail.com>
2026-07-29 14:55:52 +0530

    all data types

---
+++ basics/allDataTypes.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // int
+    int x = 7;
+    cout << x << endl;
+
+    // long
+    long y = 15;
+    cout << y << endl;
+
+    // long long
+    long long z = 150000000;
+    cout << z << endl;
+
+    // float, double
+    float a = 7.5;
+    float b = 7;
+    double c = 9;
+    cout << a << ", " << b << ", " << c << endl;
+}
